4d7970fcaaa529f36ef41afc7c35ea499d3157fa
Jesuloba <jesulobajohn@gmail.com>
2020-12-04 23:38:52 +0100

    handling styling with styled-jsx

--- components/User.js
+++ components/User.js
@@ -2,6 +2,19 @@ const user = (props) => (
 	<div>
 		<h2>{props.name}</h2>
 		<p>Age: {props.age}</p>
+		<style jsx>{`
+			div {
+				border: 1px solid #eee;
+				box-shadow: 0 2px 3px #ccc;
+				padding: 20px;
+				text-align: center;
+				margin: 20px 0;
+			}
+
+			h2 {
+				color: green;
+			}
+		`}</style>
 	</div>
 );
 
