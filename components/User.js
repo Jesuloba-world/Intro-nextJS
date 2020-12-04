aaf940bad30c1232d88817c9db36f6485b1c530f
Jesuloba <jesulobajohn@gmail.com>
2020-12-04 23:15:02 +0100

    components in next

---
+++ components/User.js
@@ -0,0 +1,8 @@
+const user = (props) => (
+	<div>
+		<h2>{props.name}</h2>
+		<p>Age: {props.age}</p>
+	</div>
+);
+
+export default user;
--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,11 @@
 import Router from "next/router";
 
+import User from "../../components/User";
+
 const authIndexPage = () => (
 	<div>
 		<h1>The Auth Page</h1>
+		<User name="John" age={25} />
 		<button onClick={() => Router.push("/")}>Go to Main</button>
 	</div>
 );
