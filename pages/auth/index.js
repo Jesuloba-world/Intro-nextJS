c1017894f74cdab369a6a2e57a2535ce69169f16
Jesuloba <jesulobajohn@gmail.com>
2020-12-05 10:23:23 +0100

    new lifecycle hooks

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,12 +2,21 @@ import Router from "next/router";
 
 import User from "../../components/User";
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
 	<div>
-		<h1>The Auth Page</h1>
+		<h1>The Auth Page - {props.appName}</h1>
 		<User name="John" age={25} />
 		<button onClick={() => Router.push("/")}>Go to Main</button>
 	</div>
 );
+
+authIndexPage.getInitialProps = (context) => {
+	const promise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve({ appName: "Super App (Auth)" });
+		}, 1000);
+	});
+	return promise;
+};
 
 export default authIndexPage;
